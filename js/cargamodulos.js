e7a833d79da9f0315431278d7a9329043bf1a788
Carlos <csvega250@gmail.com>
2018-11-03 13:22:05 -0600

    Membresias

---
+++ js/cargamodulos.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.contenido').load('html/clientes.html');
+
+    $('#clientesLink').click(function (e) { 
+        e.preventDefault();
+        $('.contenido').load('html/clientes.html');
+    });
+
+    $('#membresiasLink').click(function (e) { 
+        e.preventDefault();
+        $('.contenido').load('html/membresias.html');
+        $('.clientesLink').removeClass('active');
+        $('.membresiasLink').addClass('active');
+    });
+});
