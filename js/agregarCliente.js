319d3ed56ceddb6e4333e3e25f2ba0c7455c5b75
Luna16290847 <luis-luna98@hotmail.com>
2018-11-08 12:44:25 -0600

    Clientes mejorado

---
+++ js/agregarCliente.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('#nuevoClienteForm').submit(function(e) {
+        e.preventDefault();
+        enviar();
+        $('#nipCliente').val(" ");
+        $('#nombre').val("");
+        $('#apellidoPaterno').val("");
+        $('#apellidoMaterno').val("");
+        $('#edad').val("");
+        $('#genero').val("");
+        $('#peso').val("");
+        $('#problemas').val("");
+        $('#notas').val("");
+    });     
+    
+    function enviar() {
+        var datos = $('#nuevoClienteForm').serialize();
+        $.ajax({
+            type: "POST",
+            url: "php/agregarCliente.php",
+            data: datos,
+            success: function(data) { 
+                $('.cuerpo').load('php/cargarClientes.php');
+            }
+        });
+        
+    }
+
+});
