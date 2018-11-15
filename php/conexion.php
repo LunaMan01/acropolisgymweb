1ead007ad80a845a78f1707dae7dfa8cc3dc0823
Luna16290847 <luis-luna98@hotmail.com>
2018-11-15 09:12:36 -0600

    nuevos

--- php/conexion.php
+++ php/conexion.php
@@ -1,10 +1,7 @@
 <?php 
-
-       
-
         session_start();
         echo $_SESSION['user'];
-        $con = new mysqli("localhost", $_SESSION['user'], $_SESSION['pass'], "acropolisgym");
+        $con = new mysqli("localhost", $_SESSION['user'], $_SESSION['pass'], "acropolisgymweb");
 
         if($con->connect_errno) {
             echo "Fallo al conectar a MySQL: (" . $con->connect . ") " . $con->connect_error;
