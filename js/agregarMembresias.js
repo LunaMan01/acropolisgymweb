8f0167f887407076eb97e2ad6a3d557a16f5a6fa
CarlosVega250 <csvega250@gmail.com>
2018-11-22 02:21:09 -0600

    Mensaje al agregar membresia

--- js/agregarMembresias.js
+++ js/agregarMembresias.js
@@ -11,12 +11,59 @@ $(document).ready(function() {
             url: "php/agregarMembresia.php",
             data: datos,
             success: function(data) {
-
-
-                $('.cuerpoTabla').load('php/CargarMembresias.php');
+                if(data.indexOf("si") > -1){
+                    exito("Membresia agregada con exito");
+                    $('.cuerpoTabla').load('php/CargarMembresias.php');
+                }
+                else {
+                    error('Ocurrio un error al agregar membresia');
+                }
+                
             }
         });
         
+    }
+
+    function exito(mensaje) {
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": false,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+        toastr.success(mensaje);
+    }
+
+    function error(mensaje) {
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": false,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+        toastr.error(mensaje);
     }
 });
 
--- php/agregarMembresia.php
+++ php/agregarMembresia.php
@@ -1,10 +1,7 @@
 <?php
 include 'conexion.php';
 $conexion = $con;
-// if ($conexion->connect_errno) {
-//     echo "Fallo al conectar MySQL";
-//     exit();
-// }
+
 
 $nipCliente = $_POST['nipCliente'];
 $fechaInicio = $_POST['fechaInicio'];
@@ -12,7 +9,10 @@ $fechaFin = $_POST['fechaFin'];
 
 if(!empty($_POST["nipCliente"]))
     $query = "INSERT INTO membresias (NIP_Cliente, fecha_Inicio, fecha_Fin) VALUES (".$nipCliente.",'".$fechaInicio."','".$fechaFin."')";
-    echo $query;
     $resultado = $conexion->query($query);
-// }
+    if($resultado)
+        echo "si";
+    else
+        echo "no";
+
 ?>
